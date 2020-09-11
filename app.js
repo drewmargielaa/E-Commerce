d489aaacfdf67f58af034ecd74f3a88f1d976324
Andrew Souza <drewmargielaa@gmail.com>
2020-09-11 18:19:02 -0400

    declared all variable

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 //variables
 
 const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-clart");
+const cartDom = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const cartProducts = document.querySelector(".products-center");
+
+let cart = [];
