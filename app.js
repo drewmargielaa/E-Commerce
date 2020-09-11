2fcf25b1584579400f68fe369960ad57282b062f
Andrew Souza <drewmargielaa@gmail.com>
2020-09-11 19:35:35 -0400

    added template literals

--- app.js
+++ app.js
@@ -35,7 +35,29 @@ class Products{
 
 // display products
 class UI {
+    displayProducts(products) {
+        let result = "";
+        products.forEach(product => {
+            result += `
+            <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              Add To Bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+            `
+        })
 
+    }
 }
 
 //local storage
@@ -48,5 +70,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const products = new Products();
 
     //get all products
-    products.getProducts().then(data => console.log(data));
-})
+    products.getProducts().then(products => ui.displayProducts(products));
+});
