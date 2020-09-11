dcdde220263bb319362ef809ab3b6f3489825c29
Andrew Souza <drewmargielaa@gmail.com>
2020-09-11 18:43:55 -0400

    getProducts return title,price,id,image

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ class Products{
     async getProducts(){
         try {
             let result = await fetch('products.json');
-            return result;
+            let data = await result.json();
+            let products = data.items:
+            products = products.map(item =>{
+                const {title,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}   
+            })
+            return products
         } catch (error) {
             console.log(error);
             
@@ -38,4 +46,7 @@ class storage{
 document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const products = new Products();
+
+    //get all products
+    products.getProducts().then(data => console.log(data));
 })
